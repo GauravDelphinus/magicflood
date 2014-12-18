8c0521a96cc0362633994c8fc13726e566b0d492
ezeeideas <anukrity@Gauravs-MacBook-Pro.local>
2014-12-19 00:16:46 +0530

    About Screen Cleanup, and some Home page cleanup
    * Added Feedback, Facebook and Rate buttons on About screen and also
    added the copyright text
    * Fixed the layout of the home screen, and added the Help button (not
    yet functioning)
    * Added various required icons

--- build/Android/src/com/ezeeideas/magicflood/MFAboutActivity.java
+++ build/Android/src/com/ezeeideas/magicflood/MFAboutActivity.java
@@ -2,20 +2,32 @@ package com.ezeeideas.magicflood;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ViewFlipper;
 
-public class MFAboutActivity extends Activity
+public class MFAboutActivity extends Activity implements OnClickListener
 {
 	@Override
     protected void onCreate(Bundle savedInstanceState) 
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        
+        ImageButton facebookButton = (ImageButton) findViewById(R.id.facebook_button);
+        facebookButton.setOnClickListener(this);
+        
+        ImageButton feedbackButton = (ImageButton) findViewById(R.id.feedback_button);
+        feedbackButton.setOnClickListener(this);
+        
+        ImageButton rateButton = (ImageButton) findViewById(R.id.rate_app_button);
+        rateButton.setOnClickListener(this);
 	}
 	
     @Override
@@ -23,4 +35,30 @@ public class MFAboutActivity extends Activity
     {
         super.onDestroy(); 
     }
+
+	@Override
+	public void onClick(View v) 
+	{
+		if (v.getId() == R.id.rate_app_button)
+    	{    		
+			try 
+			{
+			    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + MFConstants.APP_PNAME)));
+			} catch (android.content.ActivityNotFoundException anfe) {
+			    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + MFConstants.APP_PNAME)));
+			}
+    	}
+		else if (v.getId() == R.id.feedback_button)
+		{    		
+			Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+					"mailto",MFConstants.SUPPORT_EMAIL, null));
+			emailIntent.putExtra(Intent.EXTRA_SUBJECT, getResources().getString(R.string.support_email_subject));
+			startActivity(Intent.createChooser(emailIntent, getResources().getString(R.string.support_email_title)));
+		}
+		else if (v.getId() == R.id.facebook_button)
+		{    		
+			Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(MFConstants.FACEBOOK_PAGE_URL));
+			startActivity(browserIntent);
+		}		
+	}
 }
---
+++ build/Android/src/com/ezeeideas/magicflood/MFConstants.java
@@ -0,0 +1,9 @@
+package com.ezeeideas.magicflood;
+
+public class MFConstants 
+{
+	// Feedback
+	public static final String SUPPORT_EMAIL = "ezeeideas@gmail.com";
+	public static final String FACEBOOK_PAGE_URL = "https://www.facebook.com/pages/Word-Jam/130607287109919";
+	public static final String APP_PNAME = "com.ezeeideas.magicflood";
+}
